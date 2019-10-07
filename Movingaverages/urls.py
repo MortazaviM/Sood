c752f037fa3ab2fc50ba5237bfd0a217104c16a4
MortazaviM <micronomicer@gmail.com>
2019-10-07 20:06:35 +0330

    adding moving averages features

--- Movingaverages/urls.py
+++ Movingaverages/urls.py
@@ -7,5 +7,5 @@ from Movingaverages.views import DataDetailView
 
 urlpatterns=[
     #url(r'get/$', views.DataAPIView.as_view(), name='get'),
-    url(r'(?P<pk>[\w:|-]+)/detail/$', views.DataDetailView.as_view(), name='detail'),
+    url(r'(?P<pk>[\w:|-]+)/detail/$', views.MovingAveragesView.as_view(), name='detail'),
 ]
--- Movingaverages/views.py
+++ Movingaverages/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from rest_framework import viewsets, generics
-from Signal.models import data
-from Signal.serializers import DataSerializer
+from Movingaverages.models import data
+from Movingaverages.serializers import DataSerializer, MovingAverageSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from Libclass.averages import Moving_Averages
 #from rest_framework.decorators import api_view
-
-
 #@api_view(['POST', 'GET'])
-
-
-
 #class DataAPIView(generics.RetrieveAPIView):
 #    lookup_field = 'pk'
 #    serializer_class = DataSerializer()
@@ -27,17 +23,41 @@ from rest_framework.views import APIView
         #else:
             #return Response(d)
 
-class DataDetailView(APIView):
-    #lookup_field = 'pk'
+class MovingAveragesView(APIView):
     def post(self, request, pk):
-        mydata=get_object_or_404(data, pk=pk)
-        Serialized_data=DataSerializer(mydata, many=True)
+        mydata=data.objects.values_list('CLOSE').filter(TICKER=pk)
+        ma55=Moving_Averages(mydata,55)
+        ma50=Moving_Averages(mydata,50)
+        ma21=Moving_Averages(mydata,21)
+        ma20=Moving_Averages(mydata,20)
+        ma10=Moving_Averages(mydata,10)
+        ma8=Moving_Averages(mydata,8)
+        output={
+            'MA55':ma55,
+            'MA50':ma50,
+            'MA21':ma21,
+            'MA20': ma20,
+            'MA10':ma10,
+            'MA8':ma8,
+            'ALEN1050':{
+                'desription':'',
+                'signal5':'',
+                'signal8':'',
+
+            },
+            'ALEN520':{
+                'desription':'',
+                'signal5':'',
+                'signal8':'',
+            }
+        }
+        Serialized_data=MovingAverageSerializer(output)
         return Response(Serialized_data.data)
 
-    def get(self, request, pk):
-        mydata=get_object_or_404(data, pk=pk)
-        Serialized_data=DataSerializer(mydata, many=True)
-        return Response(Serialized_data.data)
+    #def get(self, request, pk):
+    #    mydata=get_object_or_404(data, pk=pk)
+    #    Serialized_data=DataSerializer(mydata, many=True)
+    #   return Response(Serialized_data.data)
 
 
 
