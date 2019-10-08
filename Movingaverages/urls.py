cb407bdc6a5186a1870e8898d6384d6c8aa9663c
MortazaviM <micronomicer@gmail.com>
2019-10-08 23:15:15 +0330

    Sopport lines added [dev104]

--- Movingaverages/urls.py
+++ Movingaverages/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from Movingaverages.views import MovingAveragesView
+from Movingaverages.views import MovingAveragesView, SupportView
 
 #router = routers.DefaultRouter()
 #router.register(r'get', DataViewSet, r"get")
@@ -8,6 +8,7 @@ from Movingaverages.views import MovingAveragesView
 urlpatterns=[
     #url(r'get/$', views.DataAPIView.as_view(), name='get'),
     url(r'(?P<pk>.+?)/detail/$', MovingAveragesView.as_view(), name='detail'),
+    url(r'(?P<pk>.+?)/all/$', SupportView.as_view(), name='all'),
 ]
 
 #[\w ]+
--- Movingaverages/views.py
+++ Movingaverages/views.py
@@ -8,6 +8,7 @@ from Libclass.averages import Moving_Averages
 from Libclass.cross import Cross
 from rest_framework.pagination import PageNumberPagination
 from django.conf import settings
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 #from rest_framework.decorators import api_view
 #@api_view(['POST', 'GET'])
 #class DataAPIView(generics.RetrieveAPIView):
@@ -98,3 +99,27 @@ class MovingAveragesView(APIView, PageNumberPagination):
             return Response(Serialized_data.data)
         else:
             return Response({'Notworking'})
+
+
+
+class SupportView(APIView):
+
+    def post(self, request, pk):
+        zdata={}
+        mydata=data.objects.values_list('CLOSE').filter(TICKER=pk)
+        paginator = Paginator(mydata, 100) 
+        page = request.GET.get('page')
+        try:
+            post_list = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer deliver the first page
+            post_list = paginator.page(1)
+        except EmptyPage:
+        # If page is out of range deliver last page of results
+            post_list = paginator.page(paginator.num_pages)
+        zdata={
+                'number_of_pages':paginator.num_pages,
+                'page': page,
+                'post_list': list(post_list)
+                   }
+        return Response(data=zdata)
