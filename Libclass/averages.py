1a339450d7d92dbf8dfb89e76c478d2e888d3205
MortazaviM <micronomicer@gmail.com>
2020-01-16 16:41:35 +0330

    update

--- Libclass/averages.py
+++ Libclass/averages.py
@@ -1,26 +1,33 @@
 class Moving_Averages():
     
-    def __init__(self, data, n=14):
+    def __init__(self, data,date, n=14):
         self.data=data
+        self.date=date
         self.n=n
 
 
     def moving_average(self):
         mylist=[]
+        mydate=[]
         for i in range(0,self.n):
             mylist.append(0)
-        
-        for j in self.data:
-            mylist.append(j)
-        
-        
-        cumsum, moving_aves = [0], []
+            mydate.append(0)
 
+        for k,j in enumerate( self.data):
+            mylist.append(j)
+            mydate.append(self.date[k])
+
+        cumsum, moving_aves = [0], []
         for i, x in enumerate(mylist, 1):
             cumsum.append(cumsum[i-1] + x)
-            if i>=N:
-                moving_ave = (cumsum[i] - cumsum[i-N])/N
+            if i>=self.n*2:
+                #cumsum.append(cumsum[i-1] + x)
+                moving_ave = round((cumsum[i] - cumsum[i-self.n])/self.n ,2)
                 #can do stuff with moving_ave here
-                moving_aves.append(moving_ave)
+                moving_aves.append(
+                    {
+                        'x':mydate[i-1],
+                        'y':moving_ave,
+                        })
             
         return moving_aves
